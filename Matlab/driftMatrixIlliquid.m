71a39e00957f089698f16f6feed185559d59e864
patrick-schneider-econ <137075895+patrick-schneider-econ@users.noreply.github.com>
2024-01-12 10:54:02 +0000

    Add files via upload
    Adding functions to build transition matrices for liquid & illiquid assets, not yet designed to fit with main code nor efficient in terms of sparse construction.

---
+++ Matlab/driftMatrixIlliquid.m
@@ -0,0 +1,42 @@
+function KK = driftMatrixIlliquid(m)
+
+    global dkb dkf I J Nz
+    
+    % Preallocate some memory
+    AAi        = cell(Nz,1);
+    AAupdiag   = zeros(I*J,Nz);
+    AAlowdiag  = zeros(I*J,Nz);
+    AAcentdiag = zeros(I*J,Nz);
+    
+    chi  = -min(m,0)./dkb;
+    yy   = min(m,0)./dkb - max(m,0)./dkf;
+    zeta = max(m,0)./dkf;
+
+    for nz=1:Nz
+        %This will be the upperdiagonal of the matrix AAi
+        AAupdiag=zeros(I,1); %This is necessary because of the peculiar way spdiags is defined.
+        for j=1:J
+            AAupdiag=[AAupdiag;zeta(:,j,nz)];
+        end
+
+        %This will be the center diagonal of the matrix AAi
+        AAcentdiag= yy(:,1,nz);
+        for j=2:J-1
+            AAcentdiag=[AAcentdiag;yy(:,j,nz)];
+        end
+        AAcentdiag=[AAcentdiag;yy(:,J,nz)];
+
+        %This will be the lower diagonal of the matrix AAi
+        AAlowdiag=chi(:,2,nz);
+        for j=3:J
+            AAlowdiag=[AAlowdiag;chi(:,j,nz)];
+        end
+
+        % Add up the upper, center, and lower diagonal into a sparse matrix
+        AAi{nz} = spdiags(AAcentdiag,0,I*J,I*J)+spdiags(AAlowdiag,-I,I*J,I*J)+spdiags(AAupdiag,I,I*J,I*J);
+
+    end
+
+    KK = [AAi{1}, sparse(I*J,I*J); sparse(I*J,I*J), AAi{2}];
+
+return
---
+++ Matlab/driftMatrixLiquid.m
@@ -0,0 +1,33 @@
+function BB = driftMatrixLiquid(s)
+
+    global dbb dbf I J Nz
+    
+    % Preallocate some memory
+    BBi      = cell(Nz,1);
+    updiag   = zeros(I*J,Nz);
+    lowdiag  = zeros(I*J,Nz);
+    centdiag = zeros(I*J,Nz);
+    
+    % Prepare elements for diag inputs
+    X = -min(s,0)./dbb;
+    Y = min(s,0)./dbb - max(s,0)./dbf;
+    Z = max(s,0)./dbf;
+
+    for i = 1:Nz
+        centdiag(:,i) = reshape(Y(:,:,i),I*J,1);
+    end
+
+    lowdiag(1:I-1,:) = X(2:I,1,:);
+    updiag(2:I,:)    = Z(1:I-1,1,:);
+    for j = 2:J
+        lowdiag(1:j*I,:) = [lowdiag(1:(j-1)*I,:);squeeze(X(2:I,j,:));zeros(1,Nz)];
+        updiag(1:j*I,:)  = [updiag(1:(j-1)*I,:);zeros(1,Nz);squeeze(Z(1:I-1,j,:))];
+    end
+
+    for nz = 1:Nz
+        BBi{nz}=spdiags(centdiag(:,nz),0,I*J,I*J)+spdiags([updiag(:,nz);0],1,I*J,I*J)+spdiags([lowdiag(:,nz);0],-1,I*J,I*J);
+    end
+
+    BB = [BBi{1}, sparse(I*J,I*J); sparse(I*J,I*J), BBi{2}];
+    
+return
