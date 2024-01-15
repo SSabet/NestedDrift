492cf65a4bb5a2248b31b8296446cad9a54865a1
Patrick Schneider <schneider.patrick.m@gmail.com>
2024-01-15 18:06:29 +0000

    Add files via upload
    Made the sparse matrix construction efficient

--- Matlab/driftMatrixIlliquid.m
+++ Matlab/driftMatrixIlliquid.m
@@ -1,42 +1,36 @@
 function KK = driftMatrixIlliquid(m,dkb,dkf,par)
-
-    cellfun(@(x) assignin('caller', x, par.(x)), fieldnames(par));
-
-    % Preallocate some memory
-    AAi        = cell(Nz,1);
-    AAupdiag   = zeros(I*J,Nz);
-    AAlowdiag  = zeros(I*J,Nz);
-    AAcentdiag = zeros(I*J,Nz);
     
-    chi  = -min(m,0)./dkb;
-    yy   = min(m,0)./dkb - max(m,0)./dkf;
-    zeta = max(m,0)./dkf;
-
-    for nz=1:Nz
-        %This will be the upperdiagonal of the matrix AAi
-        AAupdiag=zeros(I,1); %This is necessary because of the peculiar way spdiags is defined.
-        for j=1:J
-            AAupdiag=[AAupdiag;zeta(:,j,nz)];
+    % Unpack parameter values
+    I = par.I; J = par.J; Nz = par.Nz;
+    
+    % Prepare elements for diag inputs
+    X = -min(m,0)./dkb;
+    Y =  min(m,0)./dkb - max(m,0)./dkf;
+    Z =  max(m,0)./dkf;
+    
+    % Build the sparse drift matrix information
+    row = []; col = []; val = [];
+    for k = 1:Nz
+            
+        diag = I*J*(k-1)+(1:I);
+        row  = [row, diag, diag];
+        col  = [col, diag, diag+I];
+        val  = [val; Y(:,1,k); Z(:,1,k)];
+        
+        for j=2:(J-1)
+            diag = I*J*(k-1) + I*(j-1)+(1:I);
+            row  = [row, diag, diag, diag];
+            col  = [col, diag, diag+I, diag-I];
+            val  = [val; Y(:,j,k); Z(:,j,k); X(:,j,k)];
         end
-
-        %This will be the center diagonal of the matrix AAi
-        AAcentdiag= yy(:,1,nz);
-        for j=2:J-1
-            AAcentdiag=[AAcentdiag;yy(:,j,nz)];
-        end
-        AAcentdiag=[AAcentdiag;yy(:,J,nz)];
-
-        %This will be the lower diagonal of the matrix AAi
-        AAlowdiag=chi(:,2,nz);
-        for j=3:J
-            AAlowdiag=[AAlowdiag;chi(:,j,nz)];
-        end
-
-        % Add up the upper, center, and lower diagonal into a sparse matrix
-        AAi{nz} = spdiags(AAcentdiag,0,I*J,I*J)+spdiags(AAlowdiag,-I,I*J,I*J)+spdiags(AAupdiag,I,I*J,I*J);
-
+        
+        diag = I*J*(k-1) + I*(J-1)+(1:I);
+        row  = [row, diag, diag];
+        col  = [col, diag, diag-I];
+        val  = [val; Y(:,J,k); X(:,J,k)];
     end
-
-    KK = [AAi{1}, sparse(I*J,I*J); sparse(I*J,I*J), AAi{2}];
-
+    
+    % ...and make a sparse matrix out of it
+    KK = sparse(row,col,val,I*J*Nz,I*J*Nz);
+    
 return
