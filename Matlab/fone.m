543ea973733f0f2291e9eafa66ca0a317646eabe
patrick-schneider-econ <137075895+patrick-schneider-econ@users.noreply.github.com>
2024-01-15 13:38:54 +0000

    Add files via upload
    Removed unnecessary unpacking of the par struct -> 10x faster!

--- Matlab/fone.m
+++ Matlab/fone.m
@@ -1,10 +1,6 @@
 function noarb = fone(b,a,zk,d,Va,forward,rb,par)
-   cellfun(@(x) assignin('caller', x, par.(x)), fieldnames(par));
-   % noarb = (rb*b + (1-xi)*w*zk - d - two_asset_kinked_cost(d,a, chi0, chi1)).^(-par.gamma).*...
-   %     (1 + chi0 * (2*forward - 1) + chi1 * d ^(chi1-1)/a) - Va;
-   % 
-   % to Patrick: why the power in front of d in 2nd line?
-   noarb = (rb*b + (1-xi)*w*zk - d - two_asset_kinked_cost(d,a, chi0, chi1)).^(-par.gamma).*...
-       (1 + chi0 * (2*forward - 1) + chi1 * d /a) - Va;
+
+   noarb = (rb*b + (1-par.xi)*par.w*zk - d - two_asset_kinked_cost(d,a, par.chi0, par.chi1)).^(-par.gamma).*...
+       (1 + par.chi0 * (2*forward - 1) + par.chi1 * d /a) - Va;
    
 end
