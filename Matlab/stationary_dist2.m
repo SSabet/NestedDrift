af54489d5658093962ae48a0e5fa16396cfd0341
Patrick Schneider <schneider.patrick.m@gmail.com>
2024-01-18 09:35:32 +0000

    Update stationary_dist2.m
    Added an alternative approach that may differ? TBC

--- Matlab/stationary_dist2.m
+++ Matlab/stationary_dist2.m
@@ -35,5 +35,33 @@ function g = stationary_dist2(A, a, b, par)
     % g_sum      = g_stacked'*ones(M,1)*da*db;
     % g_stacked  = g_stacked./g_sum;
     g = g/g_sum;
+
+    %% Alternative approach (Patrick)
+
+    M    = I*J*(Nz+1);
+    AT   = A'; 
+    
+    % Fix one value so matrix isn't singular:
+    vec        = zeros(M,1);
+    iFix       = 1657;
+    vec(iFix)  = 0.01;
+    AT(iFix,:) = [zeros(1,iFix-1),1,zeros(1,M-iFix)];
+    
+    % Solve system and rebase to get the **histogram** over the state space:
+    gtilde_stacked = AT\vec; gtilde_stacked = gtilde_stacked./sum(gtilde_stacked);
+    
+    % Prepare objects to convert the histogram to density in case grids are
+    % nonlinear. This uses a 2-dimensional equivalent of the method outlined in
+    % Section 7 of the Appendix to Achdou et al HACT paper
+    db_tilde = ones(I,1); db_tilde(2:I-1) = (b(3:I) - b(1:I-2))/2; db_tilde(1) = b(2)-b(1); db_tilde(I) = b(I)-b(I-1);
+    da_tilde = ones(J,1); da_tilde(2:J-1) = (b(3:I) - b(1:I-2))/2; da_tilde(1) = a(2)-a(1); da_tilde(J) = a(J)-a(J-1);
+    [AAA,BBB] = meshgrid(da_tilde,db_tilde); dba_tilde = reshape(AAA .* BBB,I*J,1);
+    grid_diag = spdiags(repmat(dba_tilde,Nz+1,1),0,M,M);
+    
+    % Convert the hisrogram to a density
+    g_stacked = grid_diag \ gtilde_stacked;
+    
+    % Reshape to return
+    g = reshape(g_stacked,I,J)
         
 end
