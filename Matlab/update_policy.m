ebfb7f37d0aea27b3cb1762e583716630752b808
Patrick Schneider <schneider.patrick.m@gmail.com>
2024-01-19 14:50:51 +0000

    Delete Matlab/update_policy.m

--- Matlab/update_policy.m
+++
@@ -1,7 +0,0 @@
-function [c,d] = update_policy(Va, Vb, MU_inv, a, d_bar, par)
-    cellfun(@(x) assignin('caller', x, par.(x)), fieldnames(par));
-
-    c = MU_inv(Vb);
-    d = two_asset_kinked_FOC(Va,Vb,a, chi0, chi1);
-
-end
